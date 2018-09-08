16232ab34b4709f7cb01cc2ea1c74f26c8e693a1
ptitlu04 <42698951+ptitlu04@users.noreply.github.com>
2018-09-08 19:40:30 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,15 +8,15 @@ bot.on('ready', function() {
     console.log("Connectedç");
 });
 
-bot.login(process.env.TOKEN);
+bot.login("NDg4MDI1NTA0NTY2MzQ1NzI4.DnWNZw.wa7HYBMv4zrRaw46SXZiEWVrgkI");
 
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes : \n -*help");
+        message.channel.sendMessage("Liste des commandes : \n -*help \n -*bot");
     }
 
-    if (message.content === prefix + "tt"){
+    if (message.content === prefix + "bot"){
         message.channel("Création du bot le _08/09/2018_ à _19h04_");
         console.log("Commande effectué");
     }
@@ -25,4 +25,18 @@ bot.on('message', message => {
         message.reply("Bien le bonjour. :)");
         console.log("Commande Salut effectué");
     }
+
+    if (message.content === prefix + "embed"){
+        var embed = new Discord.RichEmbed()
+            .setTitle("EMBED")
+            .setDescription("Ceci est un embed")
+            .addField(".help","Page d'aide", true)
+            .addField("Embed01", "Embed 01 ! :) Contactez ptitlu04#4274 si vous souhaitez un Bot Discord", true)
+            .setColor("0x2E64FE")
+            .setFooter("Bon moment parmis nous ! :)")
+        message.channel.sendEmbed(embed);
+    }
+
 });
+
+
