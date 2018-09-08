77bbfc5d79bb64518d9b16e3df8afe8f87787fe2
ptitlu04 <42698951+ptitlu04@users.noreply.github.com>
2018-09-08 20:09:45 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,5 +1,13 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const client = new Discord.Client();
+
+const adapter = new FileSync('database.json');
+const db = low(adapter);
+
+db.defaults({ histoires: [], xp: []}).write()
 
 var prefix = ("*")
 
@@ -35,8 +43,32 @@ bot.on('message', message => {
             .setColor("0x2E64FE")
             .setFooter("Bon moment parmis nous ! :)")
         message.channel.sendEmbed(embed);
-    }
 
-});
+    var msgauthor = message.author.id;
+
+    if(message.author.bot)return;
+
+    if(!db.get("xp").find({user: msgauthor}).value()){
+        db.get("xp").push({user : msgauthor, xp: 1}).write();
+    }else{
+        var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
+        console.log(userxpdb);
+        var userxpdb = Object.values(userxpdb)
+        console.log(userxp)
+        console.log(`Nombre d'xp: ${userxp[1]}`)
+
+        db.get("xp").find({user:msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
+    if (message.content === prefix + "xp"){
+        var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
+        var xpfinal = Object.values(xp);
+        var xp_embed = new Discord.RichEmbed()
+            .setTitle(`Stat des XP de ${message.author.username}`)
+            .setColor(`#F4D03F`)
+            .setDescription("Affichage des XP")
+            .addField("XP:", `${xpfinal[1]} xp`)
+            .setFooter("Enjoy :p")
+        message.channel.send({embed: xp_embed});
 
 
+
+}}}})
